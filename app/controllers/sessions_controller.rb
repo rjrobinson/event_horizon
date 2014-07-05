2fb43ec4ae07c46a984510470e85dae2af00f0ad
Adam Sheehan <a.t.sheehan@gmail.com>
2014-07-04 23:51:46 -0400

    Handle sign in failure.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,10 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def failure
+    redirect_to root_path, notice: "Unable to sign in."
+  end
+
   private
 
   def auth_hash
--- spec/features/guest_creates_user_account_spec.rb
+++ spec/features/guest_creates_user_account_spec.rb
@@ -2,6 +2,12 @@ require "rails_helper"
 
 feature "guest creates account" do
 
+  before :each do
+    # This suppresses any warnings from polluting the test output.
+    # http://stackoverflow.com/questions/19483367/rails-omniauth-error-in-rspec-output
+    OmniAuth.config.logger = Logger.new("/dev/null")
+  end
+
   scenario "successful sign up with valid github credentials" do
     OmniAuth.config.mock_auth[:github] = {
       "provider" => "github",
@@ -21,6 +27,18 @@ feature "guest creates account" do
     expect(page).to have_link("Sign Out", session_path)
 
     expect(User.count).to eq(1)
+  end
+
+  scenario "failure to sign up with invalid credentials" do
+    OmniAuth.config.mock_auth[:github] = :invalid_credentials
+
+    visit root_path
+
+    click_link "Sign In With GitHub"
+
+    expect(page).to have_content("Unable to sign in.")
+    expect(page).to have_link("Sign In With GitHub", new_session_path)
+    expect(User.count).to eq(0)
   end
 
 end
