29ad29b833decdce090194d66aca7337405d46ad
Adam Sheehan <a.t.sheehan@gmail.com>
2014-07-06 12:55:16 -0400

    Highlight syntax for code submissions.

--- lib/markdown_renderer.rb
+++ lib/markdown_renderer.rb
@@ -1,17 +1,7 @@
+require "code_renderer"
+
 class MarkdownRenderer < Redcarpet::Render::HTML
   def block_code(code, language)
-    lexer = Rouge::Lexer.find_fancy(language, code) || Rouge::Lexers::PlainText
-
-    # XXX HACK: Redcarpet strips hard tabs out of code blocks,
-    # so we assume you're not using leading spaces that aren't tabs,
-    # and just replace them here.
-    if lexer.tag == "make"
-      code.gsub! /^    /, "\t"
-    end
-
-    formatter = Rouge::Formatters::HTML.new(css_class: "highlight #{lexer.tag}")
-
-    html_code = formatter.format(lexer.lex(code))
-    "<div class='code-block'>#{html_code}</div>"
+    CodeRenderer.render(code, language)
   end
 end
