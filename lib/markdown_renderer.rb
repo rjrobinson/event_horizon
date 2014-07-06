3d507afba1eaff78e9185b0bd28033dd1fbd2122
Adam Sheehan <a.t.sheehan@gmail.com>
2014-07-06 11:02:00 -0400

    Style code blocks rendered through Markdown.

--- lib/markdown_renderer.rb
+++ lib/markdown_renderer.rb
@@ -1,5 +1,17 @@
-require "rouge/plugins/redcarpet"
-
 class MarkdownRenderer < Redcarpet::Render::HTML
-  include Rouge::Plugins::Redcarpet
+  def block_code(code, language)
+    lexer = Rouge::Lexer.find_fancy(language, code) || Rouge::Lexers::PlainText
+
+    # XXX HACK: Redcarpet strips hard tabs out of code blocks,
+    # so we assume you're not using leading spaces that aren't tabs,
+    # and just replace them here.
+    if lexer.tag == "make"
+      code.gsub! /^    /, "\t"
+    end
+
+    formatter = Rouge::Formatters::HTML.new(css_class: "highlight #{lexer.tag}")
+
+    html_code = formatter.format(lexer.lex(code))
+    "<div class='code-block'>#{html_code}</div>"
+  end
 end
