3ff809e7b9812214f436195e49604ec2d4dfa9d0
Adam Sheehan <a.t.sheehan@gmail.com>
2014-07-11 20:33:22 -0400

    Add full-text search for assignments.

--- spec/features/search_assignments_spec.rb
+++ spec/features/search_assignments_spec.rb
@@ -2,20 +2,20 @@ require "rails_helper"
 
 feature "search assignments" do
   scenario "successful matches with keyword" do
-    assignment_a = FactoryGirl.create(:assignment, title: "Blah", body: "foo")
-    assignment_b = FactoryGirl.create(:assignment, title: "Bloop", body: "bar")
+    assignment = FactoryGirl.create(:assignment, title: "Blah", body: "foo")
+    FactoryGirl.create(:assignment, title: "Bloop", body: "bar")
 
     visit root_path
     fill_in "query", with: "foo"
     click_button "Search"
 
     expect(page).to have_content("Found 1 result(s).")
-    expect(page).to have_link("Blah", assignment_path(assignment_a))
+    expect(page).to have_link("Blah", assignment_path(assignment))
     expect(page).to_not have_content("Bloop")
   end
 
   scenario "no results found" do
-    assignment = FactoryGirl.create(:assignment, title: "Blah", body: "foo")
+    FactoryGirl.create(:assignment, title: "Blah", body: "foo")
 
     visit root_path
     fill_in "query", with: "baz"
--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,8 +1,39 @@
 require "rails_helper"
 
 describe Assignment do
-
   let(:assignment) { FactoryGirl.create(:assignment) }
+
+  describe ".search" do
+    let!(:foo) do
+      FactoryGirl.create(:assignment,
+                         title: "Blah",
+                         body: "i like pizza")
+    end
+
+    let!(:bar) do
+      FactoryGirl.create(:assignment,
+                         title: "Shazbot",
+                         body: "Ruby sure is fun.")
+    end
+
+    let!(:baz) do
+      FactoryGirl.create(:assignment,
+                         title: "Mr. Grumblecat",
+                         body: "Ruby gems and fiddle sticks.")
+    end
+
+    it "searches by title" do
+      expect(Assignment.search("blah")).to eq([foo])
+    end
+
+    it "searches by body" do
+      expect(Assignment.search("ruby")).to eq([bar, baz])
+    end
+
+    it "ignores order of query terms" do
+      expect(Assignment.search("stick fiddle")).to eq([baz])
+    end
+  end
 
   describe ".parse" do
     let(:sample_filepath) do
@@ -24,7 +55,5 @@ describe Assignment do
       expect(assignment[:body]).
         to eq("\n\n# Blah Blah\n\nSomething goes here.\n")
     end
-
   end
-
 end
