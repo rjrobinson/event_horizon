b91c994a742ac77095f482f1ac3c3566ea8649c1
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-07 22:07:12 -0400

    Add line numbers.

--- lib/code_renderer.rb
+++ lib/code_renderer.rb
@@ -17,15 +17,15 @@ class CodeRenderer
   private
 
   def html_code_with_comments
-    lines_with_comments.join("\n")
+    numbered_lines_with_comments.join("\n")
   end
 
-  def lines_with_comments
+  def numbered_lines_with_comments
     sorted_comments = inline_comments.sort_by do |comment|
       -comment.line_number
     end
 
-    html_lines = lines
+    html_lines = numbered_lines
 
     sorted_comments.each do |comment|
       html_lines.insert(comment.line_number, format_comment(comment))
@@ -46,6 +46,14 @@ class CodeRenderer
 
   def line_count
     lines.length
+  end
+
+  def numbered_lines
+    lines.each_with_index.map do |line, index|
+      line_no = index + 1
+      "<span class=\"line\" data-line-no=\"#{line_no}\">" +
+        "<span class=\"line-no\">#{line_no}:</span>#{line}</span>"
+    end
   end
 
   def lines
