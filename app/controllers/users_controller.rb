c7ef729f7522053859272507b780d04d8b018700
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-13 10:41:38 -0400

    Show user token on settings page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authorize_admin!
+  before_action :authenticate_user!
 
   def index
     @users = User.all
--- spec/features/view_users_spec.rb
+++ spec/features/view_users_spec.rb
@@ -1,6 +1,26 @@
 require "rails_helper"
 
 feature "view users" do
+  context "as a user" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before :each do
+      sign_in_as(user)
+    end
+
+    scenario "view auth settings on own page" do
+      visit user_path(user)
+      expect(page).to have_content(user.token)
+    end
+
+    scenario "cannot view auth token for other user" do
+      other_user = FactoryGirl.create(:user)
+
+      visit user_path(other_user)
+      expect(page).to_not have_content(other_user.token)
+    end
+  end
+
   context "as an admin" do
     let(:admin) { FactoryGirl.create(:admin) }
 
@@ -27,6 +47,13 @@ feature "view users" do
       submissions.each do |submission|
         expect(page).to have_link_href(submission_path(submission))
       end
+    end
+
+    scenario "cannot view auth tokens for other user" do
+      other_user = FactoryGirl.create(:user)
+
+      visit user_path(other_user)
+      expect(page).to_not have_content(other_user.token)
     end
   end
 end
