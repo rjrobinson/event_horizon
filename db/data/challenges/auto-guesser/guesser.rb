30b3ea3cd8d2c0575042c5f86607f1939ff40e56
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-14 15:24:28 -0400

    Update sample challenges.

---
+++ db/data/challenges/auto-guesser/guesser.rb
@@ -0,0 +1,55 @@
+MAX_VALUE = 300000000
+MAX_TRIES = Math.log2(MAX_VALUE).ceil
+
+hidden = rand(MAX_VALUE) + 1
+checks_called = 0
+
+define_method :check do |guess|
+  checks_called += 1
+
+  if checks_called > MAX_TRIES + 1
+    raise "check called too many times"
+  end
+
+  if guess > hidden
+    1
+  elsif guess < hidden
+    -1
+  else
+    0
+  end
+end
+
+def guess_number(min, max)
+  # You can call the `check` method with a number to see if it
+  # is the hidden value.
+  #
+  # If the guess is correct, it will return 0.
+  # If the guess is too high, it will return 1.
+  # If the guess is too low, it will return -1.
+  #
+  # If you call `check` too many times, the program will crash.
+  #
+  # e.g. if the hidden number is 43592, then
+  #
+  # check(50000) # => 1
+  # check(40000) # => -1
+  # check(43592) # => 0
+  #
+  # When you've figured out what the hidden number is, return it
+  # from this method.
+
+  #######################
+  # YOUR CODE GOES HERE #
+  #######################
+end
+
+guess = guess_number(1, MAX_VALUE)
+
+if guess == hidden
+  puts "Guessed correctly!"
+  exit 0
+else
+  puts "Invalid guess."
+  exit 1
+end
