25648480cadffc5c896a046a6c8d2808163eddea
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-13 22:13:30 -0400

    Remove assignments from seeder.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,3 @@
-assignments_path = Rails.root.join("db/data/assignments")
-
-Dir.entries(assignments_path).each do |filename|
-  next if !filename.ends_with?(".md")
-
-  filepath = assignments_path.join(filename)
-  attr = Assignment.parse(File.read(filepath))
-
-  assignment = Assignment.find_by(slug: attr[:slug])
-  assignment ||= Assignment.new(slug: attr[:slug])
-  assignment.update!(attr)
-end
-
 challenges_path = Rails.root.join("db/data/challenges")
 
 Dir.entries(challenges_path).each do |entry|
