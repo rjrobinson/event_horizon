19b1f85af58834978f79f45a856b555a615103e4
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-20 14:01:46 -0400

    Remove specs for testing submission and download.

--- spec/features/view_challenges_spec.rb
+++ spec/features/view_challenges_spec.rb
@@ -23,7 +23,5 @@ feature "views challenges" do
     expect(page).to have_selector("p", "bar")
     expect(page).to have_selector("li", "item 1")
     expect(page).to have_selector("li", "item 2")
-
-    expect(page).to have_link("Download Files", challenge.archive.url)
   end
 end
