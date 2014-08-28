6cf4ce63ebf84ee83fff1eba0debae84ed2d4bcf
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-27 20:21:18 -0400

    Configure test and dev environments for email.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_options = { from: "test@example.com" }
+
   config.action_mailer.default_options = {
     from: "test@example.com"
   }
