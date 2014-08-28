c54b8c500464484271f9f8f9881c44a515bec9fc
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-28 10:19:42 -0400

    Additional mail settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate
   # delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {
+    host: ENV["HOST"]
+  }
+
+  config.action_mailer.default_options = {
+    from: "no-reply@#{ENV["HOST"]}"
+  }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV["SMTP_HOST"],
