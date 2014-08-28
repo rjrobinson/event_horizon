20d71c1669994629f3e10ee9ba266f9aa673fe1b
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-28 09:00:01 -0400

    Add configuration for production email settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate
   # delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_HOST"],
+    port: ENV["SMTP_PORT"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    domain: ENV["SMTP_DOMAIN"],
+    authentication: :plain
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
