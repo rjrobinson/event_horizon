ee8d0cf7557f8bd04f47a1dc503ce3ef59fe6d18
Adam Sheehan <a.t.sheehan@gmail.com>
2014-09-21 22:08:36 -0400

    Separate web server from application server.
    Use two docker containers: one running nginx as a reverse proxy
    and another running the unicorn application server. Each container
    only needs to worry about running a single process (nginx or unicorn)
    and we can take advantage of the docker restart policies if either
    one fails.

--- create-package.sh
+++ create-package.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 version=0.1
+pwd=$(pwd)
 
 rm -rf horizon-${version}
 rm -f horizon_${version}.orig.tar.gz
@@ -15,5 +16,12 @@ cd horizon-${version}
 dpkg-buildpackage
 cd ..
 
-docker build -t horizon .
-docker run -i -t horizon /bin/bash
+mkdir -p ${pwd}/deploy/web/tmp
+mkdir -p ${pwd}/deploy/app/tmp
+
+cp -f ${pwd}/horizon_${version}-1_amd64.deb ${pwd}/deploy/app/tmp/horizon_amd64.deb
+cp -f ${pwd}/horizon_${version}-1_amd64.deb ${pwd}/deploy/web/tmp/horizon_amd64.deb
+cp -f ${pwd}/deploy/nginx.conf ${pwd}/deploy/web/tmp/nginx.conf
+
+docker build -t horizon/web deploy/web
+docker build -t horizon/app deploy/app
---
+++ start-containers.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker stop app web
+docker rm -f app web
+
+docker run -d --name app --env-file=.env.production horizon/app
+docker run -d -P --name web --link app:app horizon/web
