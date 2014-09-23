d91720e83edd1dc505fae378a4314b849f2ef4ef
Adam Sheehan <a.t.sheehan@gmail.com>
2014-09-22 20:44:47 -0400

    Configure nginx to support SSL.

--- deploy/start-containers.sh
+++ deploy/start-containers.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+pwd=$(pwd)
+
 docker stop app web
 docker rm -f app web
 
 docker run -d --name app --env-file=../.env.production horizon/app
-docker run -d -P --name web --link app:app horizon/web
+docker run -d -p 80:80 -p 443:443 -v ${pwd}/ssl:/etc/nginx/ssl:ro --name web --link app:app horizon/web
