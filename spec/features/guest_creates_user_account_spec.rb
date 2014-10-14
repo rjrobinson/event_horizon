8474544dbb8b5756f97f1137bdd00a82e530b3a8
Adam Sheehan <a.t.sheehan@gmail.com>
2014-10-14 11:52:22 -0400

    Remove invalid spec.
    Since we're asking for e-mail scope we eliminate the possibility
    of not getting back an e-mail from GitHub.

--- spec/features/guest_creates_user_account_spec.rb
+++ spec/features/guest_creates_user_account_spec.rb
@@ -26,30 +26,6 @@ feature "guest creates account" do
     expect(User.count).to eq(1)
   end
 
-  scenario "failure to sign up when missing email" do
-    OmniAuth.config.mock_auth[:github] = {
-      "provider" => "github",
-      "uid" => "123456",
-      "info" => {
-        "nickname" => "boblob"
-      },
-      "credentials" => {
-        "token" => "12345"
-      }
-    }
-
-    visit root_path
-
-    click_link "Sign In With GitHub"
-
-    expect(page).to have_content("Unable to sign up.")
-    expect(page).to have_content(
-      "No public e-mail associated with GitHub account.")
-
-    expect(page).to_not have_content("Signed in as boblob")
-    expect(User.count).to eq(0)
-  end
-
   scenario "failure to sign up with invalid credentials" do
     OmniAuth.config.mock_auth[:github] = :invalid_credentials
 
