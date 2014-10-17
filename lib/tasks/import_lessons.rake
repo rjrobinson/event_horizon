65f718386ea7b63203b30449a17d7a7a67a53771
Adam Sheehan <a.t.sheehan@gmail.com>
2014-10-17 16:45:17 -0400

    Import lessons from external git repo.
    Add unit tests for importing lessons.

--- lib/tasks/import_lessons.rake
+++ lib/tasks/import_lessons.rake
@@ -1,13 +1,21 @@
 namespace :horizon do
   task import_lessons: :environment do
-    dir = Rails.root.join("db/sample_lessons")
+    curriculum_repo = ENV["CURRICULUM_GIT_REPO"] || "git@github.com:LaunchAcademy/curriculum"
 
-    Dir.entries(dir).each do |filename|
-      path = File.join(dir, filename)
+    Dir.mktmpdir("curriculum") do |tmpdir|
+      if system("git", "clone", curriculum_repo, File.join(tmpdir, "curriculum"))
+        lessons_dir = File.join(tmpdir, "curriculum", "lessons")
 
-      if File.directory?(path) && !filename.start_with?(".")
-        Lesson.import!(path)
+        Dir.entries(lessons_dir).each do |filename|
+          path = File.join(lessons_dir, filename)
+
+          if File.directory?(path) && !filename.start_with?(".")
+            Lesson.import!(path)
+          end
+        end
+      else
+        puts "Failed to clone curriculum repo."
       end
-    end
+  end
   end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -49,4 +49,42 @@ describe Lesson do
       expect(results).to include(baz)
     end
   end
+
+  describe ".import!" do
+    let(:sample_lessons_dir) { Rails.root.join("spec/data/sample_lessons") }
+
+    it "creates a new lesson" do
+      Lesson.import!(File.join(sample_lessons_dir, "expressions"))
+
+      expect(Lesson.count).to eq(1)
+
+      lesson = Lesson.find_by!(slug: "expressions")
+      expect(lesson.title).to eq("Expressions")
+      expect(lesson.type).to eq("article")
+      expect(lesson.description).to eq("bloop.")
+      expect(lesson.body).to eq("beep boop i'm an expression\n")
+      expect(lesson.position).to eq(1)
+    end
+
+    it "updates an existing lesson" do
+      FactoryGirl.create(:lesson, slug: "expressions", body: "blah")
+
+      Lesson.import!(File.join(sample_lessons_dir, "expressions"))
+
+      expect(Lesson.count).to eq(1)
+
+      lesson = Lesson.find_by!(slug: "expressions")
+      expect(lesson.body).to eq("beep boop i'm an expression\n")
+    end
+
+    it "packages an archive for a challenge" do
+      Lesson.import!(File.join(sample_lessons_dir, "rock-paper-scissors"))
+
+      expect(Lesson.count).to eq(1)
+
+      lesson = Lesson.find_by!(slug: "rock-paper-scissors")
+      expect(lesson.type).to eq("challenge")
+      expect(lesson.archive).to_not eq(nil)
+    end
+  end
 end
