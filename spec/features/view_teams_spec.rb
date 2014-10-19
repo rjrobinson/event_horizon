3ef740a82b1b26cbccb6595b5801b8c24f390325
Adam Sheehan <a.t.sheehan@gmail.com>
2014-10-19 11:29:47 -0400

    Require authentication to view teams.

--- spec/features/view_teams_spec.rb
+++ spec/features/view_teams_spec.rb
@@ -1,17 +1,35 @@
 require "rails_helper"
 
 feature "view teams" do
-  scenario "list users in a team" do
-    team = FactoryGirl.create(:team)
-    members = FactoryGirl.create_list(:team_membership, 3, team: team)
+  let(:team) { FactoryGirl.create(:team) }
 
-    visit team_path(team)
+  context "as a user" do
+    let(:user) { FactoryGirl.create(:user) }
 
-    expect(page).to have_content(team.name)
+    before :each do
+      sign_in_as(user)
+    end
 
-    members.each do |member|
-      user = member.user
-      expect(page).to have_link(user.username, href: user_path(user))
+    scenario "list users in a team" do
+      members = FactoryGirl.create_list(:team_membership, 3, team: team)
+
+      visit team_path(team)
+
+      expect(page).to have_content(team.name)
+
+      members.each do |member|
+        user = member.user
+        expect(page).to have_link(user.username, href: user_path(user))
+      end
+    end
+  end
+
+  context "as a guest" do
+    scenario "require authentication to view teams" do
+      visit team_path(team)
+
+      expect(page).to have_content("You need to sign in before continuing.")
+      expect(page).to_not have_content(team.name)
     end
   end
 end
