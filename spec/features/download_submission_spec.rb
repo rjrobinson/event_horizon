8f0b747be65767435627d8b609c37dd5897d85a7
Richard Davis <rdavis.bacs@gmail.com>
2014-10-30 11:15:03 -0400

    feature complete

--- spec/features/download_submission_spec.rb
+++ spec/features/download_submission_spec.rb
@@ -3,18 +3,20 @@ require 'rails_helper'
 feature 'admin downloads a submission', %q{
 
   As an Event Horizon chief black hole explorer,
-  I would like to retrieve the 1's and 0's of a users submission,
-  So that I might execute it locally to determine a cosmonaut's success.
+  I would like to retrieve the 1's and 0's of a
+    users submission,
+  So that I might extract and execute it locally
+    to determine a cosmonaut's success.
 
 } do
 
   let(:admin) { FactoryGirl.create(:admin) }
-  let(:submission) { FactoryGirl.create(:submission) }
+  let(:submission) { FactoryGirl.create(:submission_with_file) }
 
   scenario 'admin visits submission page, sees link to download', focus: true do
     sign_in_as(admin)
     visit submission_path(submission)
-    expect(page).to have_button('Download Submission')
+    expect(page).to have_link('Download Submission')
   end
 
 end
