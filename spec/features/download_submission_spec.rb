3ceb2a786a972e56c33680e667961bd12b743ebb
Richard Davis <rdavis.bacs@gmail.com>
2014-10-30 11:37:58 -0400

    use double-quotes. more specific link checking

--- spec/features/download_submission_spec.rb
+++ spec/features/download_submission_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-feature 'admin downloads a submission', %q{
+feature "admin downloads a submission", %q{
 
   As an Event Horizon chief black hole explorer,
   I would like to retrieve the 1's and 0's of a
@@ -13,9 +13,9 @@ feature 'admin downloads a submission', %q{
   let(:admin) { FactoryGirl.create(:admin) }
   let(:submission) { FactoryGirl.create(:submission_with_file) }
 
-  scenario 'admin visits submission page, sees link to download' do
+  scenario "admin visits submission page, sees link to download" do
     sign_in_as(admin)
     visit submission_path(submission)
-    expect(page).to have_link('Download Submission')
+    expect(page).to have_link("Download Submission", href: submission.archive.url)
   end
 end
