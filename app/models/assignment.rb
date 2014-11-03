c4b2b21d36cd284a882da55a65aacf5c26520479
Adam Sheehan <a.t.sheehan@gmail.com>
2014-11-03 16:32:45 -0500

    Display team submissions for an assignment.
    Indicates whether a submission is on time or late.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -6,4 +6,23 @@ class Assignment < ActiveRecord::Base
   validates :lesson, presence: true
   validates :required, inclusion: [true, false]
   validates :due_on, presence: true
+
+  def user_submissions
+    User.find_by_sql([USER_SUBMISSION_QUERY, due_on, lesson_id, team_id])
+  end
+
+  USER_SUBMISSION_QUERY = <<SQL
+SELECT
+  users.username,
+  subs.id AS submission_id,
+  subs.comments_count,
+  subs.created_at AS submission_time,
+  COALESCE(subs.created_at <= ?, false) AS on_time
+FROM users
+  INNER JOIN team_memberships ON users.id = team_memberships.user_id
+  INNER JOIN teams ON teams.id = team_memberships.team_id
+  LEFT OUTER JOIN (SELECT * FROM submissions WHERE lesson_id = ?) subs ON subs.user_id = users.id WHERE teams.id = ?
+ORDER BY users.username ASC, subs.created_at DESC
+SQL
+
 end
