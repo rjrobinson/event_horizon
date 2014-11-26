f7826a09e0fa4522fc532f7051755abd45543d6f
Richard Davis <rdavis.bacs@gmail.com>
2014-11-26 17:37:59 -0500

    show assignment as reviewed on the dashboard

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -7,6 +7,26 @@ class Assignment < ActiveRecord::Base
   validates :required, inclusion: [true, false]
   validates :due_on, presence: true
 
+  def self.with_status_for(user)
+    scope
+  end
+
+  def reviewed?(user)
+    Comment.count_by_sql([ADMIN_COMMENT_COUNT, lesson.id, user.id]) > 0
+  end
+
+  ADMIN_COMMENT_COUNT = <<SQL
+SELECT
+  COUNT(*)
+FROM assignments
+  JOIN lessons ON lessons.id = assignments.lesson_id
+  JOIN submissions ON submissions.lesson_id = lessons.id
+  JOIN comments ON comments.submission_id = submissions.id
+  JOIN users AS commenters ON commenters.id = comments.user_id
+  JOIN users AS submitters ON submitters.id = submissions.user_id
+WHERE commenters.role = 'admin' AND lessons.id = ? AND submitters.id = ?
+SQL
+
   def user_submissions
     User.find_by_sql([USER_SUBMISSION_QUERY, due_on, lesson_id, team_id])
   end
---
+++ spec/features/view_assignment_status_on_dashboard_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+feature "assignment status is displayed on dashboard", %q{
+
+  As a Horizon user,
+  I would like to see the status of my submissions
+  So that I am aware of what assignments I have left to do.
+
+} do
+
+  scenario "user completes assignment" do
+    team_membership = FactoryGirl.create(:team_membership)
+    user = team_membership.user
+    assignment = FactoryGirl.create(:assignment,
+      team: team_membership.team)
+    submission = FactoryGirl.create(:submission,
+      lesson: assignment.lesson, user: team_membership.user)
+
+    sign_in_as submission.user
+    visit dashboard_path
+    expect(page).to have_content("submitted")
+  end
+
+  scenario "admin reviews assignment", focus: true do
+    team_membership = FactoryGirl.create(:team_membership)
+    user = team_membership.user
+    assignment = FactoryGirl.create(:assignment,
+      team: team_membership.team)
+    submission = FactoryGirl.create(:submission,
+      lesson: assignment.lesson, user: team_membership.user)
+
+    admin = FactoryGirl.create(:user, role: "admin")
+    comment = FactoryGirl.create(:comment, user: admin, submission: submission)
+
+    sign_in_as submission.user
+    visit dashboard_path
+    expect(page).to have_content("reviewed")
+  end
+
+end
