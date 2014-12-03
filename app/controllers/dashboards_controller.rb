777f23368c876c2458f1721ed8fc3866012b6151
Richard Davis <rdavis.bacs@gmail.com>
2014-12-03 13:31:18 -0500

    authenticate user before visiting the dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,6 @@
 class DashboardsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @upcoming_assignments = current_user.assignments.order(due_on: :asc)
   end
