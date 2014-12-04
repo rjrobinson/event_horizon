ed9c8596620689b0c4de30b6c20c061edeadbd53
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-12-04 15:55:17 -0500

    dashboards controller updated for last 5 announcements; announcement and team views updated to limit to 50 for database strain

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def show
     @upcoming_assignments = current_user.assignments.order(due_on: :asc)
-    @announcements = current_user.announcements.order(created_at: :desc).limit(50)
+    @announcements = current_user.announcements.order(created_at: :desc).limit(5)
   end
 end
