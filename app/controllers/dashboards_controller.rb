f524355ba75d972fdd667ceb575675e65ec88091
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-12-04 15:37:24 -0500

    updated dashboard controller to limit db strain

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def show
     @upcoming_assignments = current_user.assignments.order(due_on: :asc)
-    @announcements = current_user.announcements.order(created_at: :desc)
+    @announcements = current_user.announcements.order(created_at: :desc).limit(50)
   end
 end
