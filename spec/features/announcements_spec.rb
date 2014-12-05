7b1c9b00e4e002a83d25686c3d056f41dab13910
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-12-05 09:48:32 -0500

    added tests for adding and deleting announcements

--- spec/features/announcements_spec.rb
+++ spec/features/announcements_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 feature "announcements" do
+  let(:team) { FactoryGirl.create(:team) }
+
   context "as a team member" do
-    let(:team) { FactoryGirl.create(:team) }
     let(:team_member) { FactoryGirl.create(:team_membership, team: team) }
 
     before :each do
@@ -36,8 +37,43 @@ feature "announcements" do
   end
 
   context "as an admin" do
-    scenario "create a new announcement"
-    scenario "fail to create a new announcement"
-    scenario "remove an existing announcement"
+    let(:admin) { FactoryGirl.create(:admin) }
+
+    before :each do
+      sign_in_as(admin)
+    end
+
+    scenario "create a new announcement" do
+      visit team_announcements_path(team)
+
+      fill_in "Title", with: "Free donuts on the counter."
+      fill_in "Description", with: "Except they're stale."
+      click_button "Add Announcement"
+
+      expect(page).to have_content("Free donuts on the counter.")
+      expect(page).to have_content("Except they're stale.")
+      expect(page).to have_content("Added announcement.")
+    end
+
+    scenario "fail to create a new announcement" do
+      visit team_announcements_path(team)
+
+      click_button "Add Announcement"
+
+      expect(page).to have_content("Failed to add announcement.")
+      expect(Announcement.count).to eq(0)
+
+    end
+
+    scenario "remove an existing announcement" do
+      announcement = FactoryGirl.create(:announcement, team: team)
+
+      visit announcement_path(announcement)
+
+      click_link "delete announcement"
+
+      expect(page).to have_content "You have deleted an announcement."
+      expect(Announcement.count).to eq(0)
+    end
   end
 end
