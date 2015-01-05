8c481badd7aa560f41e651b16eddcb1d14c8344d
Adam Sheehan <a.t.sheehan@gmail.com>
2015-01-05 10:07:55 -0500

    Move markdown rendering code to common helper.
    Markdown is used throughout the site so it is more intuitive to
    find the code in the `ApplicationHelper` rather than the
    `LessonHelper`.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def format_datetime(time)
     time.strftime("%B %e, %Y at %l:%M:%S %p")
   end
+
+  def render_markdown(content)
+    renderer.render(content).html_safe
+  end
+
+  def renderer
+    @renderer ||= Redcarpet::Markdown.new(
+      MarkdownRenderer,
+      fenced_code_blocks: true,
+      disable_indented_code_blocks: true)
+  end
 end
