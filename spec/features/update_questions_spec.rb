2c2d1517552b7e48a76c1d3e2e2f8aa91f233a87
Spencer Dixon <spencercdixon@gmail.com>
2015-01-16 11:07:58 -0500

    Users can unaccept an answer now

--- spec/features/update_questions_spec.rb
+++ spec/features/update_questions_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 feature "modifying questions" do
+
+  context "as a visitor" do
+
+  end
+
   context "as a member" do
     let(:user) { FactoryGirl.create(:user) }
 
@@ -16,7 +21,25 @@ feature "modifying questions" do
       expect(page).to have_content("Successfully deleted question")
       expect(page).to_not have_content(question.title)
     end
-    scenario "un-accept an answer"
+
+    scenario "can't delete other people's questions" do
+      question = FactoryGirl.create(:question)
+      visit question_path(question)
+
+      expect(page).to_not have_content("Delete question")
+    end
+
+    scenario "un-accept an answer", focus: true do
+      question = FactoryGirl.create(:question, user: user)
+      FactoryGirl.create(:answer, question: question)
+      visit question_path(question)
+
+      click_on "Accept Answer"
+      expect(page).to have_content("accepted answer")
+
+      click_on "Unaccept answer"
+      expect(page).to have_content("Your question has been updated.")
+    end
     scenario "change accepted answer"
   end
 end
