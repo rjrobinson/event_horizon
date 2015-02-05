713697859e4bec0e4ba7fde59d71d836137cfb24
cmkoller <cmkoller01@gmail.com>
2015-02-05 16:47:58 -0500

    Added tests for storing and retrieving calendar data with Redis

--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Calendar, type: :model do
 
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:cid) }
+
+  context "Storing/Retrieving with Redis", vcr: true  do
+
+    let(:redis) { Redis.new }
+    let(:calendar) { FactoryGirl.build(:calendar) }
+
+    after(:each) do
+      redis.flushdb
+    end
+
+    it "stores events in Redis after a call to events" do
+      calendar.events_json
+      expect(redis.keys).to include(calendar.cid)
+    end
+
+    it "retrieves events from Redis if they exist" do
+      fake_event_data = ["FAKE EVENT DATA"].to_json
+      redis.set(calendar.cid, fake_event_data)
+      expect(calendar.events_json).to eq(JSON.parse(fake_event_data))
+    end
+  end
 end
