7bb7cf3b10997a6215fc6cae7c9c3f45f2f83477
Spencer Dixon <spencercdixon@gmail.com>
2015-02-15 16:04:39 -0500

    Added presence validation for question in QuestionComment model

--- app/models/question_comment.rb
+++ app/models/question_comment.rb
@@ -3,5 +3,6 @@ class QuestionComment < ActiveRecord::Base
   belongs_to :question
 
   validates :body, presence: true
-  validates :user_id, presence: true
+  validates :user, presence: true
+  validates :question, presence: true
 end
