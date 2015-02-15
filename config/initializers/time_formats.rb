3dfcd4651630f41a5627a1801a74338606ad3c3d
Spencer Dixon <spencercdixon@gmail.com>
2015-02-15 17:28:50 -0500

    Fixed broken time display

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,3 @@
 Time::DATE_FORMATS[:agenda] = "%A, %B %-d at %l:%M %p"
 Time::DATE_FORMATS[:day_and_month] = "%A, %B %-d"
 Time::DATE_FORMATS[:hour_and_minute] = "%H:%M %p"
-Time::DATE_FORMATS[:comment_time] = "%m/%d/%y at %I:%M %P"
