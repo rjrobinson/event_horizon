f25850c69a09b021222546a54f497019889ec21f
David Tengdin <dreamingfurther@gmail.com>
2015-02-16 10:31:24 -0500

    Add decorator method and tests to encapsulate logic [#88283752]

---
+++ spec/decorators/question_decorator_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe QuestionDecorator do
+  include Draper::ViewHelpers
+
+  describe '#accepted_answer_owned_by_current_user?' do
+    let(:author) { FactoryGirl.create(:user) }
+    let(:question) { FactoryGirl.create(:question, user: author).decorate }
+    let(:answer) { double(accepted?: true) }
+
+    context 'answer is accepted' do
+      context 'user owns question' do
+        it 'should return true' do
+          allow(helpers).to receive(:current_user).and_return(author)
+          expect(question.accepted_answer_owned_by_current_user?(answer)).to eq true
+        end
+      end
+
+      context 'user does not own question' do
+        it 'should return false' do
+          allow(helpers).to receive(:current_user).and_return(FactoryGirl.create(:user))
+          expect(question.accepted_answer_owned_by_current_user?(answer)).to eq false
+        end
+      end
+    end
+
+    context 'answer is not accepted' do
+      it 'should return false' do
+        answer = double(accepted?: false)
+        expect(question.accepted_answer_owned_by_current_user?(answer)).to eq false
+      end
+    end
+  end
+end
+
